1b78b8107400598a9ab391045fee42a794f3516a
Auste <austyesir@gmail.com>
2017-12-07 08:12:27 +0200

    fixed first page

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+const Apie = ({ navigator }) => (
+  <View style={styles.container}>
+        <Text style={styles.getCvText}>Mano CV </Text>
+          <Image
+            source={
+              require('C:/Users/Auste/navigator/assets/as.jpg') 
+            }
+            style={styles.welcomeImage}
+          />
+          
+        <View style={styles.getStartedContainer}>
+          <Text style={styles.getNameText}>Austėja Sirijatavičiūtė </Text>
+          </View>
+
+          <Text style={styles.getNameText}>
+           1996-05-09
+          </Text>
+  <Text
+    style={styles.navigatorText}
+    onPress={() => navigator.push({
+      title: 'Darbovietes',
+      component: Darbovietes,
+      leftButtonTitle: 'Grįžti',
+      onLeftButtonPress: () => navigator.pop()
+    })
+    }
+  >Darbovietės > </Text>
+</View>
+)
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EEE8AA'
   },
 });
+
